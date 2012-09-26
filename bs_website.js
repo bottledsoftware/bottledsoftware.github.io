68f70cdd958adc34b3d4d39a1460105c063f1fa2
Kevin Schlieper <kevin@bottledsoftware.de>
2012-09-26 16:58:05 +0200

    Bilder für Portfolioseite

--- bs_website.js
+++ bs_website.js
@@ -61,6 +61,39 @@ $(document).ready(function() {
     
 //   $('#go-to-step-button-8').click(function() { goToSlide(8); });
    
+   $(window).bind("load", function() { 
+       
+       var footerHeight = 0,
+           footerTop = 0,
+           $footer = $(".footer-container");
+           
+       positionFooter();
+       
+       function positionFooter() {
+       
+                footerHeight = $footer.height();
+                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+       
+               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                   $footer.css({
+                        position: "absolute"
+                   }).animate({
+                        top: footerTop
+                   })
+               } else {
+                   $footer.css({
+                        position: "static"
+                   })
+               }
+               
+       }
+
+       $(window)
+               .scroll(positionFooter)
+               .resize(positionFooter)
+               
+               });
+   
    initSlides();
    refreshSlideNavigation();
    
